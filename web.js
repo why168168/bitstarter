bc5162e9736f9d4c737e93da86aaff0a59549b64
why168168 <hsiehsh2009@gmail.com>
2013-07-11 19:42:57 +0000

    web.js read index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs=require('fs');
+var buf = new Buffer(512);
+
+buf = fs.readFileSync("index.html");
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('aaaHello World!');
+  //response.send('Hello World!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
